85a150eec22dddae8170924963c1effdbb666b30
Cecilia Rivero <contact@ceciliarivero.com>
2020-04-16 12:05:16 +0100

    Refactor getLocationsDaylightHours function.

--- src/lib/getLocationsDaylightHours.ts
+++ src/lib/getLocationsDaylightHours.ts
@@ -4,24 +4,33 @@ import fetch from 'node-fetch';
 import { generateLocations } from './locations';
 import { Location, LocationDaylightData } from '../types/locations';
 
+async function getDataFromAPI(location: Location) {
+  const { lat, lng } = location;
+  const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`;
+
+  return await fetch(url, {
+    method: 'GET'
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+async function getLocationData(location: Location) {
+  const { lat, lng } = location;
+  const locationData: LocationDaylightData = await getDataFromAPI(location);
+
+  locationData['coords'] = { lat, lng };
+
+  return locationData;
+}
+
 export async function getLocationsDaylightHours() {
   const locations: Location[] = generateLocations();
 
   const locationsDaylightHours = Bluebird.map(
     locations,
-    async (location) => {
-      const { lat, lng } = location;
-      const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`;
-
-      const locationData: LocationDaylightData = await fetch(url, {
-        method: 'GET'
-      }).then((response) => {
-        return response.json();
-      });
-
-      locationData['coords'] = { lat, lng };
-
-      return locationData;
+    (location) => {
+      return getLocationData(location);
     },
     { concurrency: 5 }
   )
